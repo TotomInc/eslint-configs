31f874003e5c7b8e21d1c09c882385e5cff7afef
Thomas Cazade <cazade.thomas@gmail.com>
2020-02-09 10:37:12 +0100

    fix(typescript): add typescript resolver for custom ts paths

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -3,6 +3,8 @@ const prettierConfig = require('./prettier');
 module.exports = {
   plugins: ['eslint-comments', '@typescript-eslint', 'prettier'],
 
+  ignorePatterns: ['node_modules/', 'dist/'],
+
   extends: [
     'plugin:eslint-comments/recommended',
     'airbnb-typescript/base',
@@ -19,6 +21,18 @@ module.exports = {
     ecmaVersion: 2020,
   },
 
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.mjs', '.ts', '.d.ts', '.jsx', '.tsx', '.json'],
+      },
+    },
+  },
+
   env: {
     browser: true,
     es6: true,
@@ -26,6 +40,10 @@ module.exports = {
   },
 
   rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off',
+
     'prettier/prettier': ['error', prettierConfig],
+
+    'class-methods-use-this': 'off',
   },
 };
