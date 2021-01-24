ec5feb3d26781c28820aead000f3532a12fba53a
Thomas Cazade <cazade.thomas@gmail.com>
2021-01-24 17:25:10 +0100

    feat: disable import/prefer-default-export rule
    Explicit over implicit is better especially for larger codebases

--- packages/eslint-config-react/lib/shared-rules.js
+++ packages/eslint-config-react/lib/shared-rules.js
@@ -1,5 +1,9 @@
 const prettierrc = require('../.prettierrc');
 
 module.exports = {
+  // Explicit over implicit, especially when sharing various pieces of logic,
+  // this includes React components, JS classes, etc.
+  'import/prefer-default-export': 'off',
+
   'prettier/prettier': ['error', prettierrc],
 };
